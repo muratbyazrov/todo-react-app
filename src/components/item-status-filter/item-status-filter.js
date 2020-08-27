1e92ef2732e81982c430f716919782edaa235ee6
Murat Byazrov <murat.byazrov@yandex.ru>
2020-08-27 09:09:27 +0300

    некоторые функции компоненты отрефакторил в класс-компоненты

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,14 +1,16 @@
-import React from 'react'; // без этого вебпак не работает с JSX
+import React, { Component } from 'react'; // без этого вебпак не работает с JSX
 import './item-status-filter.css'
 
-const ItemStatusFilter = () => {
-  return (
-    <div className='btn-group'>
-      <button className='btn btn-info'>All</button>
-      <button className='btn btn-outline-secondary'>Active</button>
-      <button className='btn btn-outline-secondary'>Done</button>
-    </div>
-  )
+// если компнент работает с внутренним состоянием - используют компонент класс
+export default class ItemStatusFilter extends Component {
+  render() {
+    return (
+      <div className='btn-group'>
+        <button className='btn btn-info'>All</button>
+        <button className='btn btn-outline-secondary'>Active</button>
+        <button className='btn btn-outline-secondary'>Done</button>
+      </div>
+    )
+  }
 }
 
-export default ItemStatusFilter;
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,32 +1,59 @@
-import React from 'react'; // без этого вебпак не работает с JSX
+import React, { Component } from 'react'; // без этого вебпак не работает с JSX
 import './todo-list-item.css';
 
-// создали реакт-элемент
-// внутри props все параметры из todo-list. Нужный параметр взяли детруктуризацией
-const TodoListItem = ({ label, important = false }) => { // дали значение по умолчанию
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
+// если компнент работает с внутренним состоянием - используют компонент класс
+export default class TodoListItem extends Component { // класс наследует React.Component
+  onLabelClick = () => { // записали в ПОЛЕ КЛАССОВ, а не метод чтобы this был правильный
+    this.setState({ // state менять можно только переопределяя объект
+      done: !this.state.done
+    })
   }
-  return (
-    <span className='todo-list-item'>
-      <span
-        className='todo-list-item-label'
-        style={style}>
-        {label}
-      </span>
 
-      <button type='button'
-        className='btn btn-outline-success btn-sm float-right'>
-        <i className='fa fa-exclamation'></i>
-      </button>
+  onMarkImportant = () => {
+    this.setState((state) => { /* state асинхронный. Если важно исходное сотояние, пишем в
+      форме функции, котрая приинмает текущий state. Выше оставили как есть */
+      return {
+        important: !state.important
+      } // принимает только ту часть, которую хотим поменять. Не надо тут done
+    });
+  };
 
-      <button type='button'
-      className='btn btn-outline-danger btn-sm float-right'>
-        <i className='fa fa-trash-o'></i>
-      </button>
-    </span>)
-};
+  state = { // state хранит внутр. состояние компонента. Инициализирвть можно и в конструкторе
+    done: false,
+    important: false
+  };
 
-export default TodoListItem;
+  render() {
+    const { label } = this.props; // у класса props тут, а у ф-ци - параметры
+    const { done, important } = this.state; // см. выше - управляет состоянием
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done'
+    }
+
+    if (important) {
+      classNames += ' important'
+    }
+
+    return (
+      <span className={classNames}>
+        <span
+          className='todo-list-item-label'
+          onClick={this.onLabelClick} > {/* обработка события методом класса. on + событие*/}
+          {label}
+        </span>
+
+        <button type='button'
+          className='btn btn-outline-success btn-sm float-right'
+          onClick={this.onMarkImportant}>
+          <i className='fa fa-exclamation'></i>
+        </button>
+
+        <button type='button'
+          className='btn btn-outline-danger btn-sm float-right'>
+          <i className='fa fa-trash-o'></i>
+        </button>
+      </span>)
+  }
+}
 
